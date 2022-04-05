df20a4271420504cb056d984f8f6212bbb964c9c
Lukas Danuser <lukas.danuser@gmail.com>
2022-04-05 22:24:01 +0200

    load autoQuality setting

--- js/popupSettings.js
+++ js/popupSettings.js
@@ -1,3 +1,7 @@
 chrome.storage.sync.get(['titleTranslation'], function (result) {
     document.getElementById("translateTitle").checked = result.titleTranslation;
 });
+
+chrome.storage.sync.get(['autoQuality'], function (result) {
+    document.getElementById("autoQuality").checked = result.autoQuality;
+});
