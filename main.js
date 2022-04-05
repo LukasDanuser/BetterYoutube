7d3566f7e51c280db15d605bf27b19d96371290f
Lukas Danuser <l.danuser@rafisa.ch>
2022-04-05 16:37:56 +0200

    setup for functions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-chrome.runtime.onMessage.addListener(function (request) {
-    alert(request);
-})
+chrome.storage.sync.get(['titleTranslation'], function (result) {
+    if (result.titleTranslation == "true") {
+
+    }
+});
