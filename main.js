90c4350a6588c8cb4d54ddce54bd7c7091074cff
Lukas Danuser <l.danuser@rafisa.ch>
2022-04-06 15:35:36 +0200

    get message

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.type == "update_quality")
-        window.localStorage['ytaq_quality'] = request.quality;
-    alert(request.type)
+    if (request.type === "update_quality") {
+        window.localStorage['ytaq_quality'] = request.value;
+
+    } else if (request.type === "anti_translate") {
+    } else if (request.type === "auto_quality") {
+    } else if (request.type === "hide_shorts") {
+    }
     return true;
 });
-chrome.storage.local.get(['titleTranslation'], function (result) {
-    if (result.titleTranslation == "true") {
 
+/**chrome.storage.local.clear(function () {
+    var error = chrome.runtime.lastError;
+    if (error) {
+        console.error(error);
     }
+    // do something more
 });
+chrome.storage.sync.clear(); // callback is optional */
