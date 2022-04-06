0c2fb93ecf9bed234a9951035e1458234e4ae28e
Lukas Danuser <l.danuser@rafisa.ch>
2022-04-06 15:42:33 +0200

    added default settings

--- background.js
+++ background.js
@@ -1,2 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({ 'titleTranslation': false });
+    chrome.storage.local.set({ 'autoQuality': false });
+    chrome.storage.local.set({ 'quality': "hd1080" });
+    chrome.storage.local.set({ 'hideShorts': false });
+
 });
