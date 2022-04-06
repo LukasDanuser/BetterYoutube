5a70605a32de72c408c8541b720e717ede6f66fd
Lukas Danuser <l.danuser@rafisa.ch>
2022-04-06 13:24:03 +0200

    fix: bug where listener was not added correctly

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type == "update_quality")
+        window.localStorage['ytaq_quality'] = request.quality;
+    alert(request.type)
+    return true;
+});
 chrome.storage.local.get(['titleTranslation'], function (result) {
     if (result.titleTranslation == "true") {
 
     }
 });
-
-chrome.runtime.onMessage.addListener(function (request) {
-    if (request.type == "update_quality") {
-        window.localStorage['ytaq_quality'] = request.quality;
-        alert("t");
-    }
-});
-
